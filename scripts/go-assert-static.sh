c9fe70c8518cb9f418a8c8ec1789c9690e122557
Jacob Blain Christen <jacob@rancher.com>
2020-09-23 18:40:03 -0700

    introduce reusable scripts
    - go-build-static.sh will incorporate $GO_LDFLAGS into the built-in -ldflags,
      all other arguments as pass-through to go build
    - go-assert-static attempts to assert, via file, that the argument(s) are
      properly linked static executables
    - go-assert-boring attempts to assert, via go tool nm, that the argument(s)
      are linked against goboring

---
+++ scripts/go-assert-static.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -z "$*" ]; then
+    echo "usage: $0 file1 \[file2 ... fileN\]"
+fi
+for exe in "${@}"; do
+    if [ ! -x "${exe}" ]; then
+        echo "$exe: file not found" >&2
+        exit 1
+    fi
+    if ! file "${exe}" | grep -E '.*ELF.*executable, .*, statically linked,.*'; then
+        file "${exe}" >&2
+        echo "${exe}: not a statically linked executable" >&2
+        exit 1
+    fi
+done
